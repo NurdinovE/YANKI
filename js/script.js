e4e49102a8aa699f1c655af9e44f16fcdb651f1f
NurdinovE <nurdinovvv06@gmail.com>
2022-11-18 23:44:37 +0600

    adding catalog

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
-const hamb = document.querySelector("#hamb");
-const popup = document.querySelector("#popup");
-const menu = document.querySelector(".secondPart");
+let hamb = document.querySelector("#hamb");
+let popup = document.querySelector("#popup");
+let menu = document.querySelector(".secondPart");
 
 hamb.addEventListener("click", hambHundler);
 
@@ -53,7 +53,7 @@ window.onclick = function(e) {
 };
 $('.categories__items').slick({
     arrows: false,
-    dots: true,
+    dots: false,
     infinite: false,
     speed: 300,
     slidesToShow: 4,
@@ -85,4 +85,37 @@ $('.categories__items').slick({
     ],
 
 });
+let catUrl = ""
+catUrl.forEach((item) => {
+    function getCatUrl(){
+        let name = document.querySelector(".categories__item__name")
+        let img = document.querySelector(".categories__img")
+        return name.innerHTML = (item.name),
+                img.setAttribute("src",item.image)
+
+
+    }getCatUrl()
+})
+
+
+
+
+function myFooterFunction() {
+    document.getElementById("myFooterDropdown2").classList.toggle("show");
+    document.querySelector("#footer__dropdown__img2").classList.toggle("dropdown__up")
+}
+
+// Закрыть раскрывающийся список, если пользователь щелкнет за его пределами.
+window.onclick = function(e){
+    if (!e.target.matches('.dropbtn')) {
+        let myDropdown = document.getElementById("myFooterDropdown2");
+        let dropImg = document.getElementById("footer__dropdown__img2")
+        if (myDropdown.classList.contains('show') && dropImg.classList.contains("dropdown__up")) {
+            myDropdown.classList.remove('show');
+            dropImg.classList.remove("dropdown__up")
+
+
+        }
+    }
+};
 
